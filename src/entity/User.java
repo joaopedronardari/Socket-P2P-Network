bbd57a75e8d1a888a59103c9d297ec14fd9f9920
AdrianoDev <AdrianoDev@Adriano>
2015-10-16 00:27:59 -0300

    Cliente manda sinal de vida
    Falta testar

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,19 +1,25 @@
 package entity;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
+import socket.Server;
 
-public class User implements Comparable<User> {
+
+public class User implements Comparable<User>, Runnable {
 
 	private String userName;
-	private String password;
-	private Socket signLife; 
+	private String password; 
 	private boolean isConnect;
 	private List<User> listFriends;
 	private String ip;
+	private final int TIMEWAIT = 1000;
+	private long lastSignLife;
 	
 	public User(String userName, String password){
 		listFriends = new LinkedList<User>();
@@ -79,4 +85,32 @@ public class User implements Comparable<User> {
 		if(this.isConnect()) status = "online";
 		return this.getUserName() +" " +status;
 	}
+	
+	public void signLife(){
+		try{
+			Socket socket = new Socket(Server.ADDRES, Server.PORT);
+			DataOutputStream outServer = new DataOutputStream(socket.getOutputStream());
+			outServer.writeInt(1);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		BufferedReader msgReturnServer;
+		while(true){
+			try{
+				Socket socket = new Socket(Server.ADDRES, Server.PORT);
+				DataOutputStream outServer = new DataOutputStream(socket.getOutputStream());
+				outServer.writeInt(1);
+				msgReturnServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				this.lastSignLife = System.currentTimeMillis();
+				Thread.sleep(TIMEWAIT);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
